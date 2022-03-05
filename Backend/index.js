cae661c8cefe3f7ec0bfc48b404012308dc72351
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-03-04 23:26:10 -0800

    Initial Backend load

---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var mysql = require("mysql");
+const { addListener } = require("./database");
+//const { createConnection } = require("net");
+var connection = require('./database')
+var app = express();
+
+var bodyParser = require("body-parser");
+
+/*app.get('/',(req,res) => {
+   let sql="SELECT * from Employee_info";
+   connection.query(sql,function(err,result){
+       if(err) throw err;
+       res.send(result)
+   })
+});*/
+app.use(bodyParser.json());
+
+app.use('/',require('./routes/pages'));
+
+
+app.listen(3001, () =>{
+  console.log('App listening on port 3001');
+  connection.connect(function(err){
+      if(err)
+       throw err;
+      console.log("database connected")
+  })
+})
+
