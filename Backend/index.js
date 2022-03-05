e0db7d3bb7ee2e2188f0426c7edc0311913f077d
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-03-05 10:08:50 -0800

    login api created

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,24 @@ var express = require("express");
 var mysql = require("mysql");
 const { addListener } = require("./database");
 //const { createConnection } = require("net");
+var session = require("express-session");
 var connection = require('./database')
 var app = express();
 var cors = require("cors");
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+app.use(
+  session({
+    secret: "cmpe273_kafka_passport_mongo",
+    resave: false, // Forces the session to be saved back to the session store, even if the session was never modified during the request
+    saveUninitialized: false, // Force to save uninitialized session to db. A session is uninitialized when it is new but not modified.
+    duration: 60 * 60 * 1000, // Overall duration of Session : 30 minutes : 1800 seconds
+    activeDuration: 5 * 60 * 1000,
+  })
+);
+
+
 var bodyParser = require("body-parser");
 
 /*app.get('/',(req,res) => {
@@ -18,7 +31,7 @@ var bodyParser = require("body-parser");
 });*/
 app.use(bodyParser.json());
 
-app.use('/',require('./routes/pages'));
+app.use('/',require('./routes/authentication'));
 
 
 app.listen(3001, () =>{
