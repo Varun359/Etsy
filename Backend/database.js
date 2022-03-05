7044a3ce3e219605fca9b5714d36b246fd59c3b8
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-03-04 23:30:07 -0800

    Database connection established

---
+++ Backend/database.js
@@ -0,0 +1,10 @@
+var mysql = require("mysql")
+
+var connection= mysql.createConnection({
+    host: 'my-etsy-database.cswb01pgt66k.us-west-1.rds.amazonaws.com',
+    database: 'etsy',
+    user: 'varun',
+    password: 'password'
+})
+
+module.exports = connection
