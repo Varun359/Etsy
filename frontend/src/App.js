115afed7ab5630d9da1bf5051243b5ab0689423c
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-03-04 23:42:20 -0800

    login and Register components created

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './components/Login'
+import Register from './components/Register'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+       <div> <Login></Login></div>
+        <div> <Register></Register></div>
+    </div>
+
+
+  )
+}
+
+export default App
---
+++ frontend/src/components/Register.js
@@ -0,0 +1,48 @@
+import React,{useState} from 'react'
+
+const Register = ()=> {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [firstName,setFirstName] = useState('');
+
+    const emailChangeHandler = (e) => {
+        setEmail(e.target.value) 
+    }
+ 
+    const passwordChangeHandler = (e) => {
+        setPassword(e.target.value)
+    }
+    
+    const firstNameChangeHandler = (e) => {
+        setFirstName(e.target.value)
+    }
+    
+    return (
+     <div class="container">
+           {console.log(email,' ',password)}
+                 
+                 <div class="login-form">
+                     <div class="main-div">
+                         <div class="panel">
+                             <h2>Register</h2>
+                             <p>Please enter your details</p>
+                             {/* {this.state.invalidCredentials && <p>Invalid Credentials</p>} */}
+                         </div>
+                         
+                             <div class="form-group">
+                                 <input onChange={emailChangeHandler} value={email} type="text" class="form-control" name="email" placeholder="email"/>
+                             </div>
+                             <div class="form-group">
+                                 <input onChange={firstNameChangeHandler} value={firstName} type="text" class="form-control" name="FirstName" placeholder="First Name"/>
+                             </div>
+                             <div class="form-group">
+                                 <input onChange={passwordChangeHandler} value={password} type="password" class="form-control" name="password" placeholder="Password"/>
+                             </div>
+                             <button class="btn btn-primary">Login</button>                 
+                     </div>
+                 </div>
+             </div>
+   )
+}
+
+export default Register
