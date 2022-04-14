859719287bd4c16537894685c9b73314138832fb
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-04-14 01:14:02 -0700

    cart routes updated

--- Backend/routes/cartRoutes.js
+++ Backend/routes/cartRoutes.js
@@ -4,10 +4,13 @@ const express = require("express");
 const {
   addItemToCart,
   getCartItems,
+  addMultipleItemsToCart,
+  deleteCartItems,
 } = require("../controllers/cartController");
 const router = express.Router();
 
 router.route("/AddToCart").post(auth, addItemToCart);
 router.route("/cartItems").get(auth, getCartItems);
-
+router.route("/addAllCartItems").post(auth, addMultipleItemsToCart);
+router.route("/deleteCartItems").get(auth, deleteCartItems);
 module.exports = router;
