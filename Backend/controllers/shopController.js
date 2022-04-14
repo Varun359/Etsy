02d95b4109eee0b4b1365002024f2ba7fd1f0b7e
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-04-14 00:49:01 -0700

    shop controller updated

--- Backend/controllers/shopController.js
+++ Backend/controllers/shopController.js
@@ -6,7 +6,9 @@ const auth = require("../middlewares/auth");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const { changeUser } = require("../database");
-
+const User = require("../models/userModel");
+const Item = require("../models/itemModel");
+const { default: mongoose } = require("mongoose");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads");
@@ -20,258 +22,142 @@ const upload = multer({ dest: "uploads/" });
 
 //Check shop name
 exports.checkShopName = asyncErrorHandler(async (req, res) => {
-  console.log(req.user);
-  console.log("inside Shop Check name");
-
-  var shopCheckSql = "select shop_name from etsy.users";
-
-  connection.query(shopCheckSql, (err, result) => {
-    if (err) {
-      res.send("Error while connecting database");
-    } else {
-      console.log(result.length);
-      var check = false;
-      result.forEach((shop) => {
-        console.log(shop.shop_name);
-        if (shop.shop_name === req.body.shop_name) {
-          check = true;
-        }
-      });
-      res.send(check);
-      //res.send({ shop_name: result[0].shop_name });
-    }
-  });
+  const is_occupied = await User.findOne({ shop_name: req.body.shop_name });
+  let check = false;
+  if (is_occupied) check = true;
+  res.send(check);
 });
 
-//update shop name
+//update shop imame
 exports.updateShop = asyncErrorHandler(async (req, res) => {
-  console.log("inside Create Shop");
-  console.log("body", req.body);
-  console.log(req.files);
+  console.log("entered in shop image");
   var imageName = `${Date.now()}_${req.files.shopImage.name}`;
-  req.files.shopImage.mv(`../frontend/images/${imageName}`);
+  req.files.shopImage.mv(`../Backend/images/${imageName}`);
 
-  var updateShopSql =
-    "update etsy.users set shop_image = " +
-    mysql.escape(imageName) +
-    "where user_id=" +
-    mysql.escape(req.user.user_id);
-
-  console.log(updateShopSql);
-  connection.query(updateShopSql, (err, result) => {
-    if (err) {
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      res.send("Shop Updated");
-    }
+  const doc = await User.findByIdAndUpdate(req.user.user_id, {
+    shop_image: imageName,
   });
+  console.log(doc);
+  res.send(doc);
 });
 
 //change shop name
 exports.changeShopName = asyncErrorHandler(async (req, res) => {
-  console.log(req.user);
-  console.log("inside change shop name");
-  console.log(req.params);
-  var changeShopSql =
-    "update etsy.users set shop_name = " +
-    mysql.escape(req.body.shop_name) +
-    " where user_id =" +
-    mysql.escape(req.user.user_id);
-  console.log(changeShopSql);
-  connection.query(changeShopSql, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      res.send("Shop name changed");
-    }
+  const doc = await User.findByIdAndUpdate(req.user.user_id, {
+    shop_name: req.body.shop_name,
   });
+  console.log(doc);
+  res.send(doc);
 });
 
 //get items from the shop
 exports.getShopItems = asyncErrorHandler(async (req, res) => {
-  console.log(req.user);
-  var getItemsSql =
-    "select distinct item_id,item_name,item_price,item_desc,item_quantity,item_category,item_image,shop_name from etsy.items,etsy.users where etsy.users.user_id=etsy.items.user_id and etsy.users.user_id=" +
-    mysql.escape(req.user.user_id);
-
-  console.log("inside Get Shop Items page");
-  console.log(getItemsSql);
-  connection.query(getItemsSql, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      //    res.send({ shop_name: result[0].shop_name });
-      res.send(result);
+  const doc = await Item.find({
+    user: mongoose.Types.ObjectId(req.user.user_id),
+  }).populate("user");
+  let data = [];
+  if (doc.length) {
+    for (let item of doc) {
+      data.push({
+        _id: item._id,
+        item_name: item.item_name,
+        item_price: parseFloat(item.item_price),
+        item_desc: item.item_desc,
+        item_quantity: item.item_quantity,
+        item_category: item.item_category,
+        item_image: item.item_image ? item.item_image : null,
+        shop_name: item.user.shop_name,
+      });
     }
-  });
+  }
+  console.log(doc);
+  res.send(data);
 });
 
 //get items from the shop by Id with params
 exports.getShopItemsById = asyncErrorHandler(async (req, res) => {
-  console.log(req.user);
-  var getItemsSql =
-    "select distinct item_id,item_name,item_price,item_desc,item_quantity,item_category,item_image,shop_name from etsy.items,etsy.users where etsy.users.user_id=etsy.items.user_id and etsy.users.user_id=" +
-    mysql.escape(req.params.user_id);
-
-  console.log("inside Get Shop Items page");
-  console.log(getItemsSql);
-  connection.query(getItemsSql, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      //    res.send({ shop_name: result[0].shop_name });
-      res.send(result);
+  const doc = await Item.find({
+    user: mongoose.Types.ObjectId(req.params.user_id),
+  }).populate("user");
+  let data = [];
+  if (doc.length) {
+    for (let item of doc) {
+      data.push({
+        _id: item._id,
+        item_name: item.item_name,
+        item_price: parseFloat(item.item_price),
+        item_desc: item.item_desc,
+        item_quantity: item.item_quantity,
+        item_category: item.item_category,
+        item_image: item.item_image ? item.item_image : null,
+        shop_name: item.user.shop_name,
+      });
     }
-  });
+  }
+  console.log(doc);
+  res.send(data);
 });
 
 //add items to the shop
 exports.insertIntoShop = asyncErrorHandler(async (req, res) => {
-  console.log("inside insert into shop page");
-  console.log(req.body);
-  var insertItemsSql =
-    "insert into etsy.items (item_name,item_category, item_desc, item_price,item_quantity,user_id) values (" +
-    mysql.escape(req.body.itemName) +
-    "," +
-    mysql.escape(req.body.category) +
-    "," +
-    mysql.escape(req.body.itemDesc) +
-    "," +
-    mysql.escape(req.body.price) +
-    "," +
-    mysql.escape(req.body.quantity) +
-    "," +
-    mysql.escape(req.user.user_id) +
-    ")";
-  // where user_id = " +
-  // mysql.escape(req.user.user_id);
-
-  console.log(insertItemsSql);
-  connection.query(insertItemsSql, (err, result) => {
-    if (err) {
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      //    res.send({ shop_name: result[0].shop_name });
-      res.send("Item added to the shop successfully");
-    }
+  console.log(req.user.user_id);
+  const doc = await Item.create({
+    item_name: req.body.itemName,
+    item_category: req.body.category,
+    item_desc: req.body.itemDesc,
+    item_price: req.body.price,
+    item_quantity: req.body.quantity,
+    user: mongoose.Types.ObjectId(req.user.user_id),
   });
+
+  res.send("Item added to the shop successfully");
 });
 
 //edit an item in shop
 exports.editShopItem = asyncErrorHandler(async (req, res) => {
-  console.log(req.user);
-  console.log("inside change shop name");
-  console.log(req.params);
-  var editShopItemSql =
-    "update etsy.items set item_name = " +
-    mysql.escape(req.body.item_name) +
-    ",item_category=" +
-    mysql.escape(req.body.item_category) +
-    ",item_desc=" +
-    mysql.escape(req.body.item_desc) +
-    ",item_price=" +
-    mysql.escape(req.body.item_price) +
-    ",item_quantity =" +
-    mysql.escape(req.body.item_quantity) +
-    " where item_id=" +
-    mysql.escape(req.params.item_id);
-  //  +
-  // " and user_id=" +
-  // mysql.escape(req.user.user_id);
-  var updateQuantitySql =
-    "update etsy.items set item_quantity = item_quantity+" +
-    mysql.escape(req.body.quantity) +
-    " where item_id=" +
-    mysql.escape(req.params.item_id);
-  console.log(updateQuantitySql);
-  console.log(req.body.item_quantity);
-  connection.query(updateQuantitySql, (err, result) => {
-    if (err) {
-      res.send("Error while connecting database");
-    } else {
-      console.log("items table updated");
+  console.log("req.user");
+  //console.log(req.query);
+  console.log(req.params.item_id);
+  const doc = await Item.findByIdAndUpdate(
+    mongoose.Types.ObjectId(req.params.item_id),
+    {
+      item_name: req.body.item_name,
+      item_category: req.body.item_category,
+      item_desc: req.body.item_desc,
+      item_price: req.body.item_price,
+      item_quantity: req.body.item_quantity,
     }
-  });
-  console.log(editShopItemSql);
-  connection.query(editShopItemSql, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      res.send("Item updated");
-    }
-  });
+  );
+  console.log(doc);
+  res.send(doc);
 });
 
 //get shop details by Id
 exports.getShopDetails = asyncErrorHandler(async (req, res) => {
-  console.log(req.user);
-  var getShopDetailsSql =
-    "select shop_name,shop_image,user_image from etsy.users where etsy.users.user_id=" +
-    mysql.escape(req.user.user_id);
-
-  console.log("inside Get Shop details by Id");
-  console.log(getShopDetailsSql);
-  connection.query(getShopDetailsSql, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      //    res.send({ shop_name: result[0].shop_name });
-      res.send(result);
-    }
-  });
+  const doc = await User.find({ _id: req.user.user_id });
+  console.log(doc);
+  let data = {
+    shop_name: doc[0].shop_name ? doc[0].shop_name : null,
+    shop_image: doc[0].shop_image ? doc[0].shop_image : null,
+    user_image: doc[0].user_image ? doc[0].user_image : null,
+  };
+  res.send(data);
 });
 
 //get shop details by id
 exports.getShopDetailsById = asyncErrorHandler(async (req, res) => {
-  console.log(req.user);
-  var getShopDetailsByIdSql =
-    "select * from etsy.users where etsy.users.user_id=" +
-    mysql.escape(req.params.user_id);
-
-  console.log("inside Get Shop details by Id");
-  console.log(getShopDetailsByIdSql);
-  connection.query(getShopDetailsByIdSql, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      //    res.send({ shop_name: result[0].shop_name });
-      res.send(result);
-    }
+  const doc = await User.find({
+    _id: mongoose.Types.ObjectId(req.params.user_id),
   });
+
+  doc ? res.send(doc) : res.send("No Document");
 });
+
 //create shop
 exports.createShop = asyncErrorHandler(async (req, res) => {
-  console.log(req.user);
-  var getShopDetailsSql =
-    "update etsy.users set shop_name=" +
-    mysql.escape(req.body.shop_name) +
-    "where etsy.users.user_id=" +
-    mysql.escape(req.user.user_id);
-
-  console.log("inside Get Shop details by Id");
-  console.log(getShopDetailsSql);
-  connection.query(getShopDetailsSql, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error while connecting database");
-    } else {
-      console.log(result);
-      //    res.send({ shop_name: result[0].shop_name });
-      res.send(result);
-    }
+  console.log("Inside Create Shop", req.user.user_id);
+  const doc = await User.findByIdAndUpdate(req.user.user_id, {
+    shop_name: req.body.shop_name,
   });
+  res.send(doc);
 });
