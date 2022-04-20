c34fc2f22cbe91cda8bb76300c747c5b6dd3f74a
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-04-19 22:30:43 -0700

    pagination done for purchases page

--- frontend/src/components/PurchaseItems.js
+++ frontend/src/components/PurchaseItems.js
@@ -8,123 +8,210 @@ import _ from "lodash";
 import { BASE_URL } from "../variables";
 
 function PurchaseItems() {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [quantity, setQuantity] = useState(1);
-    const [cookie, setCookie] = useCookies(["cookie"]);
-    const [orders, setOrders] = useState([]);
-    const [groups, setGroups] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+  const [cookie, setCookie] = useCookies(["cookie"]);
+  const [orders, setOrders] = useState([]);
+  const [groups, setGroups] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get(`${BASE_URL}/orders`, {
-                headers: {
-                    "content-Type": "application/json",
-                    "auth-token": cookie.cookie.token,
-                },
-            })
-            .then((response) => {
-                console.log("Status Code : ", response.status);
-                if (response.status === 200) {
-                    console.log(response.data);
-                    setOrders(response.data);
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
-    useEffect(() => {
-        if (orders.length > 0) {
-            let data = null;
-            let list = [];
-            data = _.groupBy(orders, (item) => {
-                return item.order_id;
-            });
-            console.log("data", data);
-            console.log("groups", Object.entries(data));
-            let sample = Object.entries(data);
-            sample.forEach((item) => {
-                console.log(item);
-                list.push(item[1]);
-            });
-            //   {
-            //     groups.map((group) => console.log("group", group));
-            //   }
-            //   console.log(sample);
-            setGroups(list);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+  const [leftPage, setLeftPage] = useState(true);
+  const [rightPage, setRightPage] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/orders`, {
+        headers: {
+          "content-Type": "application/json",
+          "auth-token": cookie.cookie.token,
+        },
+      })
+      .then((response) => {
+        console.log("Status Code : ", response.status);
+        if (response.status === 200) {
+          console.log(response.data);
+          setOrders(response.data);
         }
-    }, [orders]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  useEffect(() => {
+    console.log("in second use effect");
+    if (orders.length > 0) {
+      let data = null;
+      let list = [];
+      data = _.groupBy(orders, (item) => {
+        return item.order_id;
+      });
+      console.log("data", data);
+      console.log("groups", Object.entries(data));
+      let sample = Object.entries(data);
+      sample.forEach((item) => {
+        console.log(item);
+        list.push(item[1]);
+      });
 
-    const getImgSrc = (item) => {
-        return item.item_image === null
-            ? `${BASE_URL}/images/item_image.avif`
-            : `${BASE_URL}/images/${item.item_image}`;
-    };
+      console.log("The list", list);
 
-    const changeQuantity = (value) => {
-        if (value > 0 && value < 10) {
-            setQuantity(value);
-        }
-    };
+      const startindex = (page - 1) * limit;
+      const endindex = page * limit;
+      const orders_count = list.length;
+      list = list.slice(startindex, endindex);
 
-    const handleGroupSum = (data) => {
-        let sum = 0;
-        let totalPrice = 0;
-        data.map((item) => {
-            console.log("item", item);
-            sum += item.price_buyed;
-        });
-        console.log("sum", sum);
-        return (Math.round(sum * 100) / 100).toFixed(2);
-    };
-    const itemsObject = {};
-    return (
-        <div className="purchase_container">
-            {groups.map((group) => (
-                <div className="purchase_item d-flex flex-column">
-                    <h2 className="font-weight-bold">
-                        Order placed on{" "}
-                        {new Date(group[0].dateOfPurchase).toDateString()}
-                    </h2>
-                    <h5>Order Id : #{group[0].order_id} </h5>
+      setGroups(list);
 
-                    {group.map((item) => (
-                        <div className="d-flex cart_cart">
-                            <img
-                                className="product_img_cart"
-                                src={getImgSrc(item)}
-                                alt=""
-                            />
-                            <div className="info_cart d-flex justify-content-between">
-                                <div className="purchase_item__content-width d-flex flex-column">
-                                    <h4 className="font-weight-bold">
-                                        {item.item_name}
-                                    </h4>
-                                    <h4>{item.shop_name} </h4>
-                                    <span className="mr-2">
-                                        Quantity - {item.quantity_buyed}
-                                    </span>
-                                </div>
-                                <div>
-                                    <h2 className="font-weight-bold">
-                                        ${item.price_buyed}
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+      if (page <= 1) {
+        setLeftPage(false);
+      } else {
+        setLeftPage(true);
+      }
+      if (
+        orders_count === page * limit ||
+        list.length < limit ||
+        list.length === 0
+      ) {
+        setRightPage(false);
+      } else {
+        setRightPage(true);
+      }
+    }
+  }, [orders, page, limit]);
 
-                    <div className="purchase_item_class mt-3 float-right">
-                        <h2 className="float-right">
-                            <b>Total Price : ${handleGroupSum(group)}</b>
-                        </h2>
-                    </div>
-                    <hr></hr>
+  const leftclick = () => {
+    if (!rightPage) {
+      setRightPage(true);
+    }
+    setPage(page - 1);
+  };
+
+  const rightclick = () => {
+    if (!leftPage) {
+      setLeftPage(true);
+    }
+    setPage(page + 1);
+  };
+
+  const setLimitFunction = (e) => {
+    setLimit(e.target.value);
+    setPage(1);
+  };
+
+  const getImgSrc = (item) => {
+    return item.item_image === null
+      ? `${BASE_URL}/images/item_image.avif`
+      : `${BASE_URL}/images/${item.item_image}`;
+  };
+
+  const changeQuantity = (value) => {
+    if (value > 0 && value < 10) {
+      setQuantity(value);
+    }
+  };
+
+  const handleGroupSum = (data) => {
+    let sum = 0;
+    let totalPrice = 0;
+    data.map((item) => {
+      console.log("item", item);
+      sum += item.price_buyed;
+    });
+    console.log("sum", sum);
+    return (Math.round(sum * 100) / 100).toFixed(2);
+  };
+  const itemsObject = {};
+
+  const indexOfLastOrder = currentPage * postsPerPage;
+  const indexOfFirstOrder = indexOfLastOrder - postsPerPage;
+  const currentOrders = groups.slice(indexOfFirstOrder, indexOfLastOrder);
+
+  return (
+    <div className="purchase_container">
+      {console.log("groups are", groups)}
+      <div style={{ fontSize: "1.5rem", fontWeight: "bold" }} sm={3}>
+        Select orders per page:
+        <select type="select" onChange={(e) => setLimitFunction(e)}>
+          <option key={1} value={2}>
+            {2}
+          </option>
+          <option key={2} selected value={5}>
+            {5}
+          </option>
+          <option key={3} value={10}>
+            {10}
+          </option>
+        </select>{" "}
+      </div>
+
+      <div className="float-right">
+        <button
+          disabled={!rightPage}
+          onClick={rightclick}
+          className="float-right"
+        >
+          Next
+        </button>
+      </div>
+
+      <div sm={3} className="float-right">
+        Page {page}
+      </div>
+
+      <div className="float-right">
+        <button
+          disabled={!leftPage}
+          onClick={leftclick}
+          className="float-right"
+        >
+          Prev
+        </button>
+      </div>
+
+      {groups.map((group) => (
+        <div className="purchase_item d-flex flex-column">
+          <h2 className="font-weight-bold">
+            {console.log("Date issss", group[0].date)}
+            {console.log("Order id issss", group[0].order_id)}
+            Order placed on {new Date(group[0].date).toDateString()}
+          </h2>
+          <h5>Order Id : #{group[0].order_id} </h5>
+
+          {group.map((item) => (
+            <div className="d-flex cart_cart">
+              <img className="product_img_cart" src={getImgSrc(item)} alt="" />
+              <div className="info_cart d-flex justify-content-between">
+                <div className="purchase_item__content-width d-flex flex-column">
+                  <h4 className="font-weight-bold">{item.item_name}</h4>
+                  <h4>{item.shop_name} </h4>
+                  <span className="mr-2">Quantity - {item.quantity_buyed}</span>
+                  {console.log(item.gift)}
+                  {item.gift !== null && (
+                    <span className="mr-2">Gift : {item.gift}</span>
+                  )}
                 </div>
-            ))}
+                <div>
+                  <h2 className="font-weight-bold">${item.price_buyed}</h2>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <div className="purchase_item_class mt-3 float-right">
+            <h2 className="float-right">
+              <b>Total Price : ${handleGroupSum(group)}</b>
+            </h2>
+          </div>
+          <hr></hr>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default PurchaseItems;
