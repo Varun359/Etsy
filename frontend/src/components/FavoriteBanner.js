91edf5739d5d7d2d1cdb0e42b50483faab72498d
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-04-19 22:29:08 -0700

    favorites page bug fixed

--- frontend/src/components/FavoriteBanner.js
+++ frontend/src/components/FavoriteBanner.js
@@ -7,89 +7,98 @@ import { useCookies } from "react-cookie";
 import axios from "axios";
 import { BASE_URL } from "../variables";
 function ProfileBanner() {
-    const navigate = useNavigate();
-    const [name, setName] = useState("");
-    const [cookie, setCookie] = useCookies(["cookie"]);
-    const [profileImg, setProfileImg] = useState(
-        `${BASE_URL}/images/default_avatar.png`
-    );
-    const [shopName, setShopName] = useState("");
-    const [shopImage, setShopImage] = useState(
-        `${BASE_URL}/images/shop_default.png`
-    );
-    let user = JSON.parse(localStorage.getItem("user"));
-    useEffect(() => {
-        let user = JSON.parse(localStorage.getItem("user"));
-        setName(user.first_name);
-        axios
-            .get(`${BASE_URL}/shopDetails`, {
-                headers: {
-                    "content-Type": "application/json",
-                    "auth-token": cookie.cookie.token,
-                },
-            })
-            .then((response) => {
-                console.log("Status Code : ", response.status);
-                if (response.status === 200) {
-                    console.log(response.data);
-                    setShopName(response.data[0].shop_name);
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        if (user.user_image != null)
-            setProfileImg(`${BASE_URL}/images/` + user.user_image);
-        if (user.shop_image != null)
-            setShopImage(`${BASE_URL}/images/` + user.shop_image);
-    }, []);
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [cookie, setCookie] = useCookies(["cookie"]);
+  const [profileImg, setProfileImg] = useState(
+    `${BASE_URL}/images/default_avatar.png`
+  );
+  const [shopName, setShopName] = useState("");
+  const [shopImage, setShopImage] = useState(
+    `${BASE_URL}/images/shop_default.png`
+  );
+  let user = JSON.parse(localStorage.getItem("user"));
+  console.log(user);
+  useEffect(() => {
+    //let user = JSON.parse(localStorage.getItem("user"));
+    setName(user.first_name);
+    axios
+      .get(`${BASE_URL}/shopDetails`, {
+        headers: {
+          "content-Type": "application/json",
+          "auth-token": cookie.cookie.token,
+        },
+      })
+      .then((response) => {
+        console.log("Status Code : ", response.status);
+        if (response.status === 200) {
+          console.log("in favorite Banner", response.data);
+          //   user["user_image"] = response.data.user_image;
+          //   console.log("user ", user);
+          if (response.data.user_image != null) {
+            setProfileImg(`${BASE_URL}/images/` + response.data.user_image);
+            console.log(
+              "In fav banner profile image is ",
+              response.data.user_image
+            );
+          }
+          if (response.data.shop_image != null) {
+            setShopImage(`${BASE_URL}/images/` + response.data.shop_image);
+            console.log(
+              "In fav banner shop image is ",
+              response.data.shop_image
+            );
+          }
+          setShopName(response.data.shop_name);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        <div className="favoriteBanner">
-            <div className="favoriteBanner__main">
-                <img
-                    className="favoriteBanner__main_img"
-                    src={profileImg}
-                    alt={name}
-                />
-            </div>
-            <div className="favoriteBanner__text">
-                <h1 className="favoriteBanner__text_name">{name}</h1>
-                <Edit
-                    className="favoriteBanner__text_icon"
-                    style={{ fontSize: "1.25rem" }}
-                    onClick={() => {
-                        navigate("/profile");
-                    }}
-                />
-            </div>
-            {shopName && (
-                <div
-                    onClick={() => {
-                        navigate("/shop/" + user.user_id);
-                    }}
-                    className="favoriteBanner__shop"
-                >
-                    <img
-                        className="favoriteBanner__shop_img"
-                        src={shopImage}
-                        alt={shopName}
-                    />
-                    <p className="favoriteBanner__shop_name">{shopName}</p>
-                </div>
-            )}
-            {shopName === null && (
-                <button
-                    className="btn btn-primary"
-                    onClick={(e) => {
-                        navigate("/createshop");
-                    }}
-                >
-                    <b>+</b> Create Shop
-                </button>
-            )}
+  return (
+    <div className="favoriteBanner">
+      <div className="favoriteBanner__main">
+        <img className="favoriteBanner__main_img" src={profileImg} alt={name} />
+      </div>
+      <div className="favoriteBanner__text">
+        <h1 className="favoriteBanner__text_name">{name}</h1>
+        <Edit
+          className="favoriteBanner__text_icon"
+          style={{ fontSize: "1.25rem" }}
+          onClick={() => {
+            navigate("/profile");
+          }}
+        />
+      </div>
+      {shopName && (
+        <div
+          onClick={() => {
+            navigate("/shop/" + user.user_id);
+          }}
+          className="favoriteBanner__shop"
+        >
+          <img
+            className="favoriteBanner__shop_img"
+            src={shopImage}
+            alt={shopName}
+          />
+          <p className="favoriteBanner__shop_name">{shopName}</p>
         </div>
-    );
+      )}
+      {shopName === null && (
+        <button
+          className="btn btn-primary"
+          onClick={(e) => {
+            navigate("/createshop");
+          }}
+        >
+          <b>+</b> Create Shop
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default ProfileBanner;
--- frontend/src/components/FavoriteList.js
+++ frontend/src/components/FavoriteList.js
@@ -7,134 +7,131 @@ import { useCookies } from "react-cookie";
 import { BASE_URL } from "../variables";
 
 function FavoriteList() {
-    var [data, setData] = useState([]);
-    var [dashBoardData, setDashBoardData] = useState([]);
-    const [cookie, setCookie] = useCookies(["cookie"]);
-    const [refresh, setRefresh] = useState(true);
-    const [search, setSearch] = useState("");
-    const removeItemId = (itemId) => {
-        setRefresh(!refresh);
-    };
+  var [data, setData] = useState([]);
+  var [dashBoardData, setDashBoardData] = useState([]);
+  const [cookie, setCookie] = useCookies(["cookie"]);
+  const [refresh, setRefresh] = useState(true);
+  const [search, setSearch] = useState("");
+  const removeItemId = (itemId) => {
+    setRefresh(!refresh);
+  };
 
-    useEffect(() => {
-        axios
-            .get(`${BASE_URL}/favoriteItems`, {
-                headers: {
-                    "content-Type": "application/json",
-                    "auth-token": cookie.cookie.token,
-                },
-            })
-            .then((response) => {
-                console.log(response.data);
-                setData(response.data);
-                console.log(data);
-                var dashBoardData_dummy = [];
-                if (response.data.length === 0) {
-                    dashBoardData_dummy = "nothing to show";
-                    setDashBoardData(dashBoardData_dummy);
-                } else {
-                    dashBoardData_dummy = response.data.map((item) => {
-                        var ImageSrc =
-                            item.item_image === null
-                                ? `${BASE_URL}/images/item_image.avif`
-                                : `${BASE_URL}/images/${item.item_image}`;
-                        var dashBoardItem = (
-                            <DashBoardItem
-                                key={item.item_id}
-                                src={ImageSrc}
-                                name={item.item_name}
-                                shopName={item.shop_name}
-                                price={item.item_price}
-                                currency={"$"}
-                                isFavorite={true}
-                                itemId={item.item_id}
-                                removeElement={removeItemId}
-                            />
-                        );
-                        if (item.item_name === null) {
-                            return null;
-                        }
-                        return dashBoardItem;
-                    });
-                    setDashBoardData(dashBoardData_dummy);
-                }
-            });
-    }, [refresh]);
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/favoriteItems`, {
+        headers: {
+          "content-Type": "application/json",
+          "auth-token": cookie.cookie.token,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+        console.log(data);
+        var dashBoardData_dummy = [];
+        if (response.data.length === 0) {
+          dashBoardData_dummy = "nothing to show";
+          setDashBoardData(dashBoardData_dummy);
+        } else {
+          dashBoardData_dummy = response.data.map((item) => {
+            var ImageSrc =
+              item.item_image === null
+                ? `${BASE_URL}/images/item_image.avif`
+                : `${BASE_URL}/images/${item.item_image}`;
+            var dashBoardItem = (
+              <DashBoardItem
+                key={item.item_id}
+                src={ImageSrc}
+                name={item.item_name}
+                shopName={item.shop_name}
+                price={item.item_price}
+                currency={"$"}
+                isFavorite={true}
+                itemId={item.item_id}
+                removeElement={removeItemId}
+              />
+            );
+            if (item.item_name === null) {
+              return null;
+            }
+            return dashBoardItem;
+          });
+          setDashBoardData(dashBoardData_dummy);
+        }
+      });
+  }, [refresh]);
 
-    const searchResults = () => {
-        axios
-            .get(`${BASE_URL}/searchFavoriteItems/${search}`, {
-                headers: {
-                    "content-Type": "application/json",
-                    "auth-token": cookie.cookie.token,
-                },
-            })
-            .then((response) => {
-                console.log(response.data);
-                setData(response.data);
-                console.log(data);
-                var dashBoardData_dummy = [];
-                if (response.data.length === 0) {
-                    dashBoardData_dummy = "nothing to show";
-                    setDashBoardData(dashBoardData_dummy);
-                } else {
-                    dashBoardData_dummy = response.data.map((item) => {
-                        var ImageSrc =
-                            item.item_image === null
-                                ? `${BASE_URL}/images/item_image.avif`
-                                : `${BASE_URL}/images/${item.item_image}`;
-                        var dashBoardItem = (
-                            <DashBoardItem
-                                key={item.item_id}
-                                src={ImageSrc}
-                                name={item.item_name}
-                                shopName={item.shop_name}
-                                price={item.item_price}
-                                currency={"$"}
-                                isFavorite={true}
-                                itemId={item.item_id}
-                                removeElement={removeItemId}
-                            />
-                        );
-                        if (item.item_name === null) {
-                            return null;
-                        }
-                        return dashBoardItem;
-                    });
-                    setDashBoardData(dashBoardData_dummy);
-                }
-            });
-    };
-    return (
-        <div className=" favoriteList__container">
-            <div className="favoriteList__header">
-                <h2 className="favoriteList__h2">Favorite items</h2>
-                <form
-                    className="favoriteList_search"
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        searchResults();
-                    }}
-                >
-                    <input
-                        placeholder="search for favorites"
-                        className="favoriteList__input"
-                        value={search}
-                        onChange={(e) => {
-                            setSearch(e.target.value);
-                        }}
-                    />
-                    <button
-                        type="submit"
-                        className="border-0 favoriteList__search_icon"
-                    >
-                        <Search />
-                    </button>
-                </form>
-            </div>
-            <div className="dashBoardItem">{dashBoardData}</div>
-        </div>
-    );
+  const searchResults = () => {
+    axios
+      .get(`${BASE_URL}/searchFavoriteItems/${search}`, {
+        headers: {
+          "content-Type": "application/json",
+          "auth-token": cookie.cookie.token,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+        console.log(data);
+        var dashBoardData_dummy = [];
+        if (response.data === 0) {
+          dashBoardData_dummy = "nothing to show";
+          setDashBoardData(dashBoardData_dummy);
+        } else {
+          dashBoardData_dummy = response.data.map((item) => {
+            var ImageSrc =
+              item.item_image === null
+                ? `${BASE_URL}/images/item_image.avif`
+                : `${BASE_URL}/images/${item.item_image}`;
+            var dashBoardItem = (
+              <DashBoardItem
+                key={item.item_id}
+                src={ImageSrc}
+                name={item.item_name}
+                shopName={item.shop_name}
+                price={item.item_price}
+                currency={"$"}
+                isFavorite={true}
+                itemId={item.item_id}
+                removeElement={removeItemId}
+              />
+            );
+            if (item.item_name === null) {
+              return null;
+            }
+            return dashBoardItem;
+          });
+          setDashBoardData(dashBoardData_dummy);
+        }
+      });
+  };
+  return (
+    <div className=" favoriteList__container">
+      <div className="favoriteList__header">
+        <h2 className="favoriteList__h2">Favorite items</h2>
+        <form
+          className="favoriteList_search"
+          onSubmit={(e) => {
+            e.preventDefault();
+            searchResults();
+          }}
+        >
+          <input
+            placeholder="search for favorites"
+            className="favoriteList__input"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          <button type="submit" className="border-0 favoriteList__search_icon">
+            <Search />
+          </button>
+        </form>
+      </div>
+      <div className="dashBoardItem">{dashBoardData}</div>
+    </div>
+  );
 }
 
 export default FavoriteList;
