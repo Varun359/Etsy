d353936560eeddc9e26a3446b6d2a1cd57363f4f
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-05-15 18:41:06 -0700

    getCartItems,getPreviousOrders, getShopItems, getShopItemsById, getItemDetails

--- graphql_backend/Graphql/Query.js
+++ graphql_backend/Graphql/Query.js
@@ -1,4 +1,11 @@
-const { User, Items, Cart } = require("./TypeDef");
+const {
+  User,
+  Items,
+  Cart,
+  Cart_Rel,
+  Purchases_Rel,
+  Items_Rel,
+} = require("./TypeDef");
 //const UserController = require("../controller/User");
 const {
   GraphQLSchema,
@@ -12,6 +19,8 @@ const {
 const itemsDb = require("../models/itemModel.js");
 const usersDb = require("../models/userModel.js");
 const userFavorites = require("../models/userFavoritesModel");
+const cartDb = require("../models/cartModel.js");
+const purchaseDb = require("../models/purchasesModel");
 const { default: mongoose } = require("mongoose");
 const query = new GraphQLObjectType({
   name: "query",
@@ -73,6 +82,61 @@ const query = new GraphQLObjectType({
         const doc = await usersDb.find({ _id: args.user_id });
         console.log(doc[0]);
         return doc[0];
+      },
+    },
+    getCartItems: {
+      type: new GraphQLList(Cart_Rel),
+      async resolve(parent, args, req) {
+        const documents = await cartDb
+          .find({
+            user: mongoose.Types.ObjectId(req.user.user_id),
+          })
+          .populate("item user");
+
+        console.log("Documents ", documents);
+        return documents;
+      },
+    },
+    getPreviousOrders: {
+      type: new GraphQLList(Purchases_Rel),
+      async resolve(parent, args, req) {
+        return await purchaseDb
+          .find({
+            user: mongoose.Types.ObjectId(req.user.user_id),
+          })
+          .populate("order");
+      },
+    },
+    getShopItems: {
+      type: new GraphQLList(Items_Rel),
+      async resolve(parent, args, req) {
+        return await itemsDb
+          .find({
+            user: mongoose.Types.ObjectId(req.user.user_id),
+          })
+          .populate("user");
+      },
+    },
+    getShopItemsById: {
+      type: new GraphQLList(Items_Rel),
+      args: {
+        user_id: { type: GraphQLString },
+      },
+      async resolve(parent, args, req) {
+        return await itemsDb
+          .find({
+            user: mongoose.Types.ObjectId(args.user_id),
+          })
+          .populate("user");
+      },
+    },
+    getItemDetails: {
+      type: Items_Rel,
+      args: {
+        item_id: { type: GraphQLString },
+      },
+      async resolve(parent, args, req) {
+        return await itemsDb.findById(args.item_id).populate("user");
       },
     },
     getAllItemsById: {
