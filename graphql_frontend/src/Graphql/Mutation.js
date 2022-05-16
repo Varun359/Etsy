52ce3dcd9fda4238842f6669a06a357af5eb35e9
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-05-16 03:33:53 -0700

    update user profile mutation in frontend

--- graphql_frontend/src/Graphql/Mutation.js
+++ graphql_frontend/src/Graphql/Mutation.js
@@ -10,10 +10,38 @@ import { gql } from "@apollo/client";
 // `;
 
 export const UPDATE_USER_PROFILE = gql`
-  mutation userProfile($first_name: String , $email: String , $gender: String , $city: String , $phone_no:String , $country:String ,$DOB:String ,$about:String ,$address: String ) {
-    userProfile(first_name : $first_name, email : $email, gender: $gender, city:$city, phone_no:$phone_no, country : $country, DOB : $DOB, about:$about, address : $address  )) {
+  mutation editProfile(
+    $user_id: String
+    $first_name: String
+    $email: String
+    $gender: String
+    $city: String
+    $phone_no: String
+    $country: String
+    $DOB: String
+    $about: String
+    $address: String
+  ) {
+    editProfile(
+      user_id: $user_id
+      first_name: $first_name
+      email: $email
+      gender: $gender
+      city: $city
+      phone_no: $phone_no
+      country: $country
+      DOB: $DOB
+      about: $about
+      address: $address
+    ) {
       first_name
+      email
       country
+      city
+      phone_no
+      DOB
+      about
+      address
       gender
     }
   }
